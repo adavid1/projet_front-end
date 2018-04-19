900ed1c4d326519cd70a99786ee4e495f5d20416
axeldvi.pro <axeldvi.pro@gmail.com>
2018-04-19 14:00:01 -0400

    add http get

--- src/app/meteo/meteo.component.ts
+++ src/app/meteo/meteo.component.ts
@@ -16,30 +16,9 @@ export class MeteoComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.townService.getTown(1).subscribe(town => {
+    this.townService.getTown(3).subscribe(town => { //Modifier ici pour une ville différente
       this.town=town;
     });
   }
 
 }
-
-/*import { Component, OnInit } from '@angular/core';
-import { Town } from '../town';
-
-@Component({
-  selector: 'app-meteo',
-  templateUrl: './meteo.component.html',
-  styleUrls: ['./meteo.component.css']
-})
-export class MeteoComponent implements OnInit
-{
-  
-  
-  town: Town = {id: 1, name: "belfort", temperature:0, icon: "rain"};
-  //town: Town = {id: 2 , name "New-York": , temperature:22 , icon: "cloudy"};
-  
-  constructor() { }
-
-  ngOnInit() { }
-
-}*/
--- src/app/town.service.ts
+++ src/app/town.service.ts
@@ -14,27 +14,3 @@ export class TownService {
     return this.http.get<Town>(`${this.URL}/${id}.json`);
   }
 }
-
-/*import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import { Town } from './town';
-import {environment} from '../environments/environment';
-
-const TOWN_API_URL = environment.TOWN_API_URL;
-
-
-@Injectable()
-export class TownService {
-    
-  constructor(private http: HttpClient) { }
-
-  
-getTown(id : number) : Observable<Town> {
-    return this.http.get<Town[]>(TOWN_API_URL)
-  }
-  
-  
-  }
-
-}*/
